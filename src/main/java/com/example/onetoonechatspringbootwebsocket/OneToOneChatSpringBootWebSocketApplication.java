01fe95f023bdc12f8ad50a29f7c1332f61795c8f
Koba <kobaabzianidze@gmail.com>
2024-04-28 16:18:18 +0400

    Initial commit

---
+++ src/main/java/com/example/onetoonechatspringbootwebsocket/OneToOneChatSpringBootWebSocketApplication.java
@@ -0,0 +1,13 @@
+package com.example.onetoonechatspringbootwebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneToOneChatSpringBootWebSocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneToOneChatSpringBootWebSocketApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/onetoonechatspringbootwebsocket/user/Status.java
@@ -0,0 +1,5 @@
+package com.example.onetoonechatspringbootwebsocket.user;
+
+public enum Status {
+    ONLINE, OFFLINE
+}
